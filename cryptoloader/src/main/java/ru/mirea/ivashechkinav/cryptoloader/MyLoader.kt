38cc7d08993069240fef127db987be7467077171
1INFINITY <praecantatio@mail.ru>
2023-04-29 14:48:54 +0300

    crypto loader

---
+++ cryptoloader/src/main/java/ru/mirea/ivashechkinav/cryptoloader/MyLoader.kt
@@ -0,0 +1,57 @@
+package ru.mirea.ivashechkinav.cryptoloader
+
+import android.content.Context
+import android.os.Bundle
+import android.os.SystemClock
+import androidx.loader.content.AsyncTaskLoader
+import java.security.InvalidKeyException
+import java.security.NoSuchAlgorithmException
+import javax.crypto.*
+import javax.crypto.spec.SecretKeySpec
+
+
+class MyLoader(context: Context, args: Bundle?) : AsyncTaskLoader<String?>(context) {
+    private var firstName: String? = null
+
+    init {
+
+        if (args != null) {
+            val cryptText = args.getByteArray(ARG_WORD)
+            val key = args.getByteArray("key")!!
+            val originalKey: SecretKey = SecretKeySpec(key, 0, key.size, "AES")
+
+            firstName = decryptMsg(cryptText, originalKey)
+        }
+    }
+
+    override fun onStartLoading() {
+        super.onStartLoading()
+        forceLoad()
+    }
+
+    override fun loadInBackground(): String? {
+        SystemClock.sleep(5000)
+        return firstName
+    }
+    fun decryptMsg(cipherText: ByteArray?, secret: SecretKey?): String? {
+/* Decrypt the message */
+        return try {
+            val cipher = Cipher.getInstance("AES")
+            cipher.init(Cipher.DECRYPT_MODE, secret)
+            String(cipher.doFinal(cipherText))
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        } catch (e: NoSuchPaddingException) {
+            throw RuntimeException(e)
+        } catch (e: IllegalBlockSizeException) {
+            throw RuntimeException(e)
+        } catch (e: BadPaddingException) {
+            throw RuntimeException(e)
+        } catch (e: InvalidKeyException) {
+            throw RuntimeException(e)
+        }
+    }
+    companion object {
+        const val ARG_WORD = "word"
+    }
+}
